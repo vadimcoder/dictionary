a5d606e194cb7c1cf1e7ec3fc48794eccf25bd11
Vadim <439939+vadimcoder@users.noreply.github.com>
2024-01-16 12:49:17 +0400

    address codeclimate

--- src/components/IrregularVerbs/index.tsx
+++ src/components/IrregularVerbs/index.tsx
@@ -3,50 +3,52 @@ import { T_RECORD } from "../../db/types";
 import { Player } from "../Player/Player";
 import { WordWithLinks } from "../WordWithLinks";
 
+function getStyle(gridColumn: number, gridRow: number) {
+  return { gridColumn, gridRow };
+}
+
 export function IrregularVerbs({ record }: { record: T_RECORD }) {
   const { dateAdded } = record;
 
   return (
     <div className={"IrregularVerbs"}>
-      <div style={{ gridColumn: 1, gridRow: 1 }}>
+      <div style={getStyle(1, 1)}>
         <Player word={record.wordSet.word} />
       </div>
 
-      <div style={{ gridColumn: 2, gridRow: 1 }}>
+      <div style={getStyle(2, 1)}>
         <WordWithLinks word={record.wordSet.word} dateAdded={dateAdded} />
       </div>
 
-      <div style={{ gridColumn: 4, gridRow: 1 }}>
+      <div style={getStyle(4, 1)}>
         <Player word={record.irregularVerb!.secondForm.word} />
       </div>
 
-      <div style={{ gridColumn: 5, gridRow: 1 }}>
+      <div style={getStyle(5, 1)}>
         <WordWithLinks
           word={record.irregularVerb!.secondForm.word}
           dateAdded={dateAdded}
         />
       </div>
 
-      <div style={{ gridColumn: 7, gridRow: 1 }}>
+      <div style={getStyle(7, 1)}>
         <Player word={record.irregularVerb!.thirdForm.word} />
       </div>
 
-      <div style={{ gridColumn: 8, gridRow: 1 }}>
+      <div style={getStyle(8, 1)}>
         <WordWithLinks
           word={record.irregularVerb!.thirdForm.word}
           dateAdded={dateAdded}
         />
       </div>
 
-      <div style={{ gridColumn: 2, gridRow: 2 }}>
-        {record.wordSet.transcription}
-      </div>
+      <div style={getStyle(2, 2)}>{record.wordSet.transcription}</div>
 
-      <div style={{ gridColumn: 5, gridRow: 2 }}>
+      <div style={getStyle(5, 2)}>
         {record.irregularVerb!.secondForm.transcription}
       </div>
 
-      <div style={{ gridColumn: 8, gridRow: 2 }}>
+      <div style={getStyle(8, 2)}>
         {record.irregularVerb!.thirdForm.transcription}
       </div>
     </div>
