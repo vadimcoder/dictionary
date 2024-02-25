02525f1da1d47d1172b8d9fa17b34137eb3d52ae
Vadim <439939+vadimcoder@users.noreply.github.com>
2024-02-25 22:19:01 +0400

    add material tabs

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,14 +1,55 @@
 import "./style.css";
-import { NavLink } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { DirSwitcher } from "./DirSwitcher/DirSwitcher";
+import { useTheme } from "@mui/material/styles";
+import { Tab, Tabs } from "@mui/material";
+
+function a11yProps(index: number) {
+  return {
+    id: `simple-tab-${index}`,
+    "aria-controls": `simple-tabpanel-${index}`,
+  };
+}
 
 export function NavBar() {
+  const { pathname } = useLocation();
+  const theme = useTheme();
+
   return (
-    <nav>
-      <NavLink to={"/"}>All</NavLink>
-      <NavLink to={"/latest"}>Latest</NavLink>
-      <NavLink to={"/quiz"}>Quiz</NavLink>
-      <NavLink to={"/dev"}>Dev</NavLink>
+    <nav style={{ borderBottom: `1px solid ${theme.palette.divider}` }}>
+      <div>
+        <Tabs value={pathname} aria-label="navigation">
+          <Tab
+            label="All"
+            {...a11yProps(0)}
+            value={"/"}
+            to={"/"}
+            component={Link}
+          />
+          <Tab
+            label="Latest"
+            {...a11yProps(1)}
+            value={"/latest"}
+            to={"/latest"}
+            component={Link}
+          />
+          <Tab
+            label="Quiz"
+            {...a11yProps(2)}
+            value={"/quiz"}
+            to={"/quiz"}
+            component={Link}
+          />
+          <Tab
+            label="Dev"
+            {...a11yProps(3)}
+            value={"/dev"}
+            to={"/dev"}
+            component={Link}
+          />
+        </Tabs>
+      </div>
+
       <DirSwitcher />
     </nav>
   );
