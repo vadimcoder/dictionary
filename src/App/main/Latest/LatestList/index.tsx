9044ef65b49ba0e28c08b2e253b2d88275b7d449
Vadim <439939+vadimcoder@users.noreply.github.com>
2024-03-03 13:58:22 +0300

    add shuffle

--- src/App/main/Latest/LatestList/index.tsx
+++ src/App/main/Latest/LatestList/index.tsx
@@ -1,20 +1,16 @@
 import { TBodyAssociation } from "../TBodyAssociation";
-import { DB } from "../../../../db/db";
-import { T_ROW } from "../../../../db/types";
+import { T_ROW, T_ROWS } from "../../../../db/types";
 import { Tr } from "../../../../components/Tr";
 import "./style.css";
-import { useGlobalState } from "../../../../GlobalState/GlobalState";
 
-export function LatestList() {
-  const [globalState] = useGlobalState();
-
+export function LatestList({ rows }: { rows: T_ROWS }) {
   return (
     <table className={"LatestList"}>
-      {DB.getLastRows(globalState.latestLastRowsCount).map((row: T_ROW) =>
+      {rows.map((row: T_ROW) =>
         row.associations ? (
           <TBodyAssociation row={row} key={row.wordSet.word} />
         ) : (
-          <tbody className={"TBodySimple"} key={row.wordSet.word}>
+          <tbody key={row.wordSet.word}>
             <Tr row={row} />
           </tbody>
         ),
--- src/App/main/Latest/index.tsx
+++ src/App/main/Latest/index.tsx
@@ -1,16 +1,31 @@
+import { Button } from "@mui/material";
 import { LatestSelector } from "./LatestSelector/LatestSelector";
 import { LatestList } from "./LatestList";
 import { GLOBAL_NAV_ARIA } from "../../NavBar";
+import "./LatestPanel.css";
+import { useShuffle } from "./useShuffle";
 
 export function Latest() {
+  const [rows, shuffleRows] = useShuffle();
+
   return (
     <div
       id={GLOBAL_NAV_ARIA.latest.contentId}
       aria-labelledby={GLOBAL_NAV_ARIA.latest.tabId}
     >
-      <LatestSelector />
+      <div className={"LatestPanel"}>
+        <LatestSelector />
 
-      <LatestList />
+        <Button
+          onClick={shuffleRows}
+          variant="contained"
+          className={"LatestPanel__button"}
+        >
+          Shuffle rows
+        </Button>
+      </div>
+
+      <LatestList rows={rows} />
     </div>
   );
 }
