cbef48d6b1ce3c0dc95550ac714811950bb7d4d6
Vadim <439939+vadimcoder@users.noreply.github.com>
2025-04-16 20:43:38 +0400

    comment out Timer

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -1,14 +1,13 @@
 import "./style.css";
 import { DirSwitcher } from "./DirSwitcher/DirSwitcher";
 import { TopMenu } from "./TopMenu";
-import { Timer } from "../Timer";
 
 export function NavBar() {
   return (
     <nav>
       <TopMenu />
 
-      <Timer />
+      {/*<Timer />*/}
 
       <DirSwitcher />
     </nav>
